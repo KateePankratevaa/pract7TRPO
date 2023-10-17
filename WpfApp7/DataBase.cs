078f601dc9b1823b913dc4f3838f03d631714a0a
KateePankratevaa <124819002+KateePankratevaa@users.noreply.github.com>
2023-10-17 14:53:02 +0500

    Add files via upload

---
+++ WpfApp7/DataBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp7
+{
+    class DataBase
+    {
+        private static DataBase database;
+        private ToursDataSetTableAdapters.ToursTableAdapter toursTable;
+        private ToursDataSet toursData;
+        private DataBase()
+        {
+            toursTable = new ToursDataSetTableAdapters.ToursTableAdapter();
+            toursData = new ToursDataSet();
+            toursTable.Fill(toursData.Tours);
+        }
+             public static DataBase DatabaseInstance
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new DataBase();
+                }
+                return database;
+            }
+        }
+
+        public ToursDataSetTableAdapters.ToursTableAdapter ToursTableAdapter
+        {
+            get
+            {
+                return toursTable;
+            }
+        }
+
+        public ToursDataSet ToursData
+        {
+            get
+            {
+                return toursData;
+            }
+        }
+    }
+    }
+    
+   
+
+
